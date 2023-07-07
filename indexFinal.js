36a677b094106623a2f6ead0eb1fbea82e1152e3
SandraZSequeira <135245320+SandraZSequeira@users.noreply.github.com>
2023-07-07 16:32:35 +0100

    more nav updates

--- indexFinal.js
+++
@@ -1,9 +0,0 @@
-//Navbar pop-up box
-
-let menu =document.querySelector(`#menu-icon`);
-let navbar =document.querySelector(`.navbar`);
-
-menu.onclick =()  => {
-    menu.classList.toggle(`bx-x`);
-    navbar.classList.toggle(`open`);
-}
